3a7ae9009120892b348f94d58127168b43e70c67
pragmaticAweds <jimohafeez738@gmail.com>
2025-03-04 11:32:20 +0100

    feat: api build process

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,10 @@ import Footer from "@/components/ui/landing-page/footer";
 import Section2 from "@/components/ui/landing-page/section-2";
 import Section3 from "@/components/ui/landing-page/section-3";
 
-export default function Home() {
+export default async function Home() {
+  // const res = await fetch(`http://localhost:3000/api/wallets`);
+  // const data = await res.json();
+
   return (
     <main className="space-y-20 md:space-y-52 md:pb-20 container">
       <Hero />
