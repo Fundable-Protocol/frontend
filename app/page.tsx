ebeddaf963c63cc82fcd5be43c2996638b7b52b2
pragmaticAweds <jimohafeez738@gmail.com>
2025-03-08 22:55:47 +0100

    feat: completed saving address

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,6 @@ import Section2 from "@/components/ui/landing-page/section-2";
 import Section3 from "@/components/ui/landing-page/section-3";
 
 export default async function Home() {
-  // const res = await fetch(`http://localhost:3000/api/wallets`);
-  // const data = await res.json();
-
   return (
     <main className="space-y-20 md:space-y-52 md:pb-20 container">
       <Hero />
